ed6297ef7d5aa2b46224c5ea979caa5d3e29df94
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-12-19 07:47:51 +0530

    event handling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,29 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  public nw_name = 'IT MASTER';
+  public str = ''
   title = 'new_project';
+  obj = {
+    title1 : 'angular',
+    title2 : 'node',
+    title3 : 'frontend framework'
+  }
+  ngOnInit=()=>{
+    console.log(this.str)
+  }
+  colorFunction = () =>{
+    return 'yellow'
+  }
+  heightFunction=()=>{
+    return "25px"
+  }
+  functionCalling(){
+    console.log('button clicked..!')
+    this.str = 'Good Morning'
+  }
+  functionCalling2(){
+    console.log('second button clicked')
+    this.str = 'Good AfterNoon'
+  }
 }
