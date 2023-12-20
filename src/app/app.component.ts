caa95ae2549a760429b5a66d8576f91f31afecda
vaishnav janardhanan <vaishnavjanardhanan22@gmail.com>
2023-12-20 07:42:40 +0530

    routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,73 +6,73 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  public nw_name = 'IT MASTER';
-  public str = ''
-  public count1 = 0
-  public nw_str = ''
-  public is_hidden = true
-  check(){
-    this.is_hidden = false
-    console.log('button clicked...!')
-    if(this.count1 % 2 == 0){
-      this.nw_str = 'even';
-    }else{
-      this.nw_str = 'odd';
-    }
-  }
-  clear(){
-    this.is_hidden = true
-  }
-  title = 'new_project';
-  obj = {
-    title1 : 'angular',
-    title2 : 'node',
-    title3 : 'frontend framework'
-  }
-  public Evenodd = 'even'
-  ngOnInit=()=>{
-    console.log(this.str)
-  }
-  colorFunction = () =>{
-    return 'yellow'
-  }
-  heightFunction=()=>{
-    return "25px"
-  }
-  functionCalling(){
-    console.log('button clicked..!')
-    this.str = 'Good Morning'
-  }
-  functionCalling2(){
-    console.log('second button clicked')
-    this.str = 'Good AfterNoon'
-  }
-  public count = 0
-  Addition=()=> {
-    console.log('add button clicked')
-    this.count = this.count + 1 
-    if(this.count % 2 == 0){
-      this.Evenodd = 'even'
-    }else{
-      this.Evenodd = 'odd'
-    }
-  }
-  Substraction = () =>{
-    console.log('substraction button clicked ')
-    this.count = this.count-1
-    if(this.count % 2 == 0){
-      this.Evenodd = 'even'
-    }else{
-      this.Evenodd = 'odd'
-    }
-  }
-  // for if
-  public fruit = [
-    {'name':"orange","price":100},
-    {'name':"mango","price":200},
-    {'name':"pinapple","price":150},
-    {'name':"watermelon","price":400},
-    {'name':"lemon","price":130}
-  ]
+  // public nw_name = 'IT MASTER';
+  // public str = ''
+  // public count1 = 0
+  // public nw_str = ''
+  // public is_hidden = true
+  // check(){
+  //   this.is_hidden = false
+  //   console.log('button clicked...!')
+  //   if(this.count1 % 2 == 0){
+  //     this.nw_str = 'even';
+  //   }else{
+  //     this.nw_str = 'odd';
+  //   }
+  // }
+  // clear(){
+  //   this.is_hidden = true
+  // }
+  // title = 'new_project';
+  // obj = {
+  //   title1 : 'angular',
+  //   title2 : 'node',
+  //   title3 : 'frontend framework'
+  // }
+  // public Evenodd = 'even'
+  // ngOnInit=()=>{
+  //   console.log(this.str)
+  // }
+  // colorFunction = () =>{
+  //   return 'yellow'
+  // }
+  // heightFunction=()=>{
+  //   return "25px"
+  // }
+  // functionCalling(){
+  //   console.log('button clicked..!')
+  //   this.str = 'Good Morning'
+  // }
+  // functionCalling2(){
+  //   console.log('second button clicked')
+  //   this.str = 'Good AfterNoon'
+  // }
+  // public count = 0
+  // Addition=()=> {
+  //   console.log('add button clicked')
+  //   this.count = this.count + 1 
+  //   if(this.count % 2 == 0){
+  //     this.Evenodd = 'even'
+  //   }else{
+  //     this.Evenodd = 'odd'
+  //   }
+  // }
+  // Substraction = () =>{
+  //   console.log('substraction button clicked ')
+  //   this.count = this.count-1
+  //   if(this.count % 2 == 0){
+  //     this.Evenodd = 'even'
+  //   }else{
+  //     this.Evenodd = 'odd'
+  //   }
+  // }
+  // // for if
+  // public fruit = [
+  //   {'name':"orange","price":100},
+  //   {'name':"mango","price":200},
+  //   {'name':"pinapple","price":150},
+  //   {'name':"watermelon","price":400},
+  //   {'name':"lemon","price":130}
+  // ]
 
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HomeComponent } from './home/home.component';
+import { ContactUsComponent } from './contact-us/contact-us.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HomeComponent,
+    ContactUsComponent
   ],
   imports: [
     BrowserModule,
